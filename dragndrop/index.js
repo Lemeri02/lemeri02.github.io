36aa9ab797448209c76f7d9c0a978aedaaa23282
I_Iskuzhin <44976339+Lemeri02@users.noreply.github.com>
2019-07-19 00:14:56 +0500

    jq
    sj

--- dragndrop/index.js
+++ dragndrop/index.js
@@ -1,4 +1,3 @@
- 
 fetch('https://kodaktor.ru/cart_data.json')
 .then( (result) => result.json())
 .then( ({hdd, sdd, usbdrive}) => {
@@ -7,4 +6,21 @@ fetch('https://kodaktor.ru/cart_data.json')
     document.getElementById('usbPrice').textContent = usbdrive;
     console.log(hdd);
 });
- 
+
+$(function() {
+    $('#drag, #drag2').draggable({
+        containment: 'parent',
+        cursor: 'move',
+         
+        //helper: 'clone',
+        revert: true,
+    });
+
+    $('#box').droppable({
+        drop:function(event, ui) {
+            $(this).addClass("ui-state-highlight").find('> span').html('OK!');
+
+
+        }
+    });
+});
